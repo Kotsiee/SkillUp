ca4d5f514b5543fd4856f62c1ab91dd00733f0fa
Kotsiee <66284889+Kotsiee@users.noreply.github.com>
2025-04-13 05:04:12 +0100

    deployment test 1

--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+if (!Deno.env.get('SUPABASE_URL')) {
+  await import('$std/dotenv/load.ts');
+}
 
-import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
-import config from "./fresh.config.ts";
+import { start } from '$fresh/server.ts';
+import manifest from './fresh.gen.ts';
+import config from './fresh.config.ts';
 
 await start(manifest, config);
